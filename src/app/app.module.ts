3f918ff11f7fccd7b8fbc95e6c435810e1a54b1b
Maycon Jardim <mayconsmill@gmail.com>
2023-01-06 11:55:17 -0300

    Componente trade machine criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TradeMachineComponent } from './components/trade-machine/trade-machine.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TradeMachineComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/trade-machine/trade-machine.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'trade-machine',
+  templateUrl: './trade-machine.component.html',
+  styleUrls: ['./trade-machine.component.scss'],
+})
+export class TradeMachineComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
